25018a21bcc95cf9ae6362974020bb61a51355d2
cmkwananzi-ui <cmkwananzi@gmail.com>
2026-05-04 09:53:04 +0200

    Create tailwind.config.js for Nyabawa Legal Practice project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/**
+ * Tailwind CSS Configuration File
+ */
+
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
